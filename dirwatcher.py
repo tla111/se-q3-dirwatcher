05fdf9b98404ff853c7f566ce88b414517f7eb15
Tim La <email@example.com>
2020-10-08 16:23:43 -0400

    looping over file in directories

--- dirwatcher.py
+++ dirwatcher.py
@@ -9,16 +9,26 @@ import sys
 import argparse
 import signal
 import time
+import os
+
+exit_flag = False
 
 
 def search_for_magic(filename, start_line, magic_string):
-    # Your code here
+    # file_dict = {}
+    # with open(filename, 'r') as file:
+    #     file.read()
     return
 
 
 def watch_directory(path, magic_string, extension, interval):
-    # Your code here
-    return
+    file_dict = {}
+    directories = os.listdir(os.path.abspath(path))
+    while not exit_flag:
+        time.sleep(interval)
+        for files in directories:
+            file_dict[files] = 0
+        print(file_dict)
 
 
 def create_parser():
@@ -27,13 +37,13 @@ def create_parser():
     parser = argparse.ArgumentParser(
         description="Find magic word"
     )
-    # parser.add_argument('-file', '--File',
-    #                     help='Tell user to insert filename when running dirwatcher.py')
-    # parser.add_argument('magicword', help='Tell user to look up magicword)
-
+    parser.add_argument(
+        '-file', help='adds file extension to end of file', default='.txt')
+    parser.add_argument('dir', help='Specifies directory')
+    parser.add_argument('magicword', help='Tell user to look up magicword')
+    parser.add_argument(
+        '-interval', help='Determines processing speed', type=float, default=1)
     return parser
-    # python dirwatcher.py magic/file.txt bananas
-    return
 
 
 def signal_handler(sig_num, frame):
@@ -48,6 +58,28 @@ def main(args):
     if not ns:
         parser.print_usage()
         sys.exit(1)
+
+     # Hook into these two signals from the OS
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    # Now my signal_handler will get called if OS sends
+    # either of these to my process.
+
+    while not exit_flag:
+        try:
+            watch_directory(ns.dir, ns.magicword, ns.file, ns.interval)
+            pass
+        except Exception as e:
+            # This is an UNHANDLED exception
+            # Log an ERROR level message here
+            pass
+
+        # put a sleep inside my while loop so I don't peg the cpu usage at 100%
+        time.sleep(polling_interval)
+
+    # final exit point happens here
+    # Log a message that we are shutting down
+    # Include the overall uptime since program start
     return
 
 
